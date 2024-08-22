ca46c74e0be3aee58b11865a3f1888e3d9ffd0ee
Phillipe John Jules <phillipejohnjules@gmail.com>
2024-08-21 22:36:31 -0400

    validation complete
    the validation is complete and now adding final stylistic tweaks

--- landing_page.js
+++ landing_page.js
@@ -1,10 +1,37 @@
 const userName = document.getElementById("username")
 const password = document.getElementById("password")
+const badPassword = document.getElementById("bad_password")
+const badName = document.getElementById("bad_name")
+const subButton = document.getElementById("button")
 
 function userNameValidation(entry) {
-  if (entry === ""){
-    alert("User name cannot be empty!");
+  if (entry === "" || entry.length < 4){
+    alert("Must be longer than 4 characters!");
     return false;
   }
   return true;
 }
+
+function passwordValidation(secret){
+  if (secret === "" || secret.length < 4){
+    badPassword.textContent = "Must be longer than 4 characters!"
+    return false;
+  }
+  return true
+}
+
+function handleClick(event) {
+  event.preventDefault();
+
+  const isUserNameValid = userNameValidation(userName.value)
+
+  const isPasswordValid = passwordValidation(password.value)
+
+  if (isUserNameValid && isPasswordValid) {
+    window.open("admin_page/admin_dashboard.html"); // Open the admin dashboard if both validations pass
+  } else {
+    // Handle the case where either the username or password is invalid
+    console.log("Invalid input");
+  }}
+
+subButton.addEventListener("click", handleClick)
